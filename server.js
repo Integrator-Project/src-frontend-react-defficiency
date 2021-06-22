cc615bf31b6b304f3facad6a5aca9db4b403c177
Alexandre Ferreira de Lima <alex.ferreiradelima64@gmail.com>
2021-06-21 21:38:59 -0300

    :bug: Change build to public in server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(favicon(__dirname + '/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port);
